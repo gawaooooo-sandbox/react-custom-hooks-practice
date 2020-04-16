43944b088e8ce41b29ae12a9a27413ddc33e2d5f
gawao <gawao57@gmail.com>
2020-04-16 13:55:23 +0900

    v1 カスタムフック未使用

---
+++ src/Page.tsx
@@ -0,0 +1,56 @@
+// Viewとロジックが混在しているため、読みづらく、ロジックのテストが難しいコード
+
+import React, {useState} from 'react'
+
+export const Page = () => {
+    const topPage = 1
+    const lastPage = 4
+    const initHistoy: number[] = [topPage]
+    const [history, setHistory] = useState<number[]>(initHistoy)
+
+    const currentPage = history[history.length - 1]
+
+    return (
+        <div>
+            <div>現在のページ: {currentPage}</div>
+            <button onClick={() => {
+                // 現在のトップページの場合は移動しない
+                if (currentPage !== topPage) {
+                    const nextHistory = [...history, topPage]
+                    setHistory(nextHistory)
+                }
+            }}>トップ</button>
+            <button onClick={() => {
+                const nextPage = currentPage + 1
+                // ラストページより先には進めない
+                if (nextPage <= lastPage) {
+                    const nextHistory = [...history, nextPage]
+                    setHistory(nextHistory)
+                }
+            }}>
+                次へ
+            </button>
+            <button onClick={() => {
+                // トップページより前には戻れない
+                if (history.length > 1) {
+                    const nextHistory = [...history.slice(0, history.length - 1)]
+                    setHistory(nextHistory)
+                }
+            }}>
+                戻る
+            </button>
+            <button onClick={() => {
+                // 現在ラストページの場合は移動しない
+                if (currentPage !== lastPage) {
+                    const nextHistory = [...history, lastPage]
+                    setHistory(nextHistory)
+                }
+            }}>
+                ラスト
+            </button>
+            <button onClick={() => {
+                setHistory(initHistoy)
+            }}>履歴を消去</button>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { Page } from './Page';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Page />
   </React.StrictMode>,
   document.getElementById('root')
 );
